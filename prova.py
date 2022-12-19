82c14b7a6e3f19e84809b6da9a2dbe8a20427fcc
StellaCentis <stella.centis@gmail.com>
2022-12-19 16:14:47 +0100

    Imported lists

--- prova.py
+++ prova.py
@@ -1,1 +1,13 @@
-print(3)
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(8,8))
+plt.show()
+
+def says_hello():
+    print('hello')
+
+says_hello()
+
+new_series = pd.Series([1,2,3])
+print('series: ', new_series)
